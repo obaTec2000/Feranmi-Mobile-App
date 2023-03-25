43f6750e6d53b29185a87e0826e2f87d11598d88
Obanijesu <obanijesujohn45@gmail.com>
2023-03-25 03:41:49 +0100

    Artist G

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import {View, Text} from "react-native";
+import Artist from './componenets/screens/Artist'
+import MyCart from './componenets/screens/MyCart'
+import ProductInfo from "./componenets/screens/ProductInfo";
+
 
 const App = () => {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+   <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Artist" component={Artist} />
+      <Stack.Screen name="MyCart" component={MyCart} />
+      <Stack.Screen name="ProductInfo" component={ProductInfo} />
+    </Stack.Navigator>
+   </NavigationContainer> 
 
   );
 };
